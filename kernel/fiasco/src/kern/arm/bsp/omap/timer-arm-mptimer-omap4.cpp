4239a0e015341fba7ba1b61b93bbe52ed59762b8
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2013-07-18 10:49:37 +0200

    FOC: Rate OMAP4 timer at 350 MHz
    Only valid for PandaBoard ES2 where u-boot sets the MPU clock to 700 MHz (of
    possible 1200 MHz) only.

--- kernel/fiasco/src/kern/arm/bsp/omap/timer-arm-mptimer-omap4.cpp
+++ kernel/fiasco/src/kern/arm/bsp/omap/timer-arm-mptimer-omap4.cpp
@@ -4,5 +4,13 @@ INTERFACE [arm && mptimer && omap4_pandaboard]:
 EXTENSION class Timer
 {
 private:
-  static Mword interval() { return 499999; }
+  static Mword interval()
+  {
+    /*
+     * This is only valid for Panda ES2, as we run it at our offices. For ES2
+     * U-Boot will clock the board at 700 MHz leading to a 350 MHz private timer
+     * tick.
+     */
+    return 349999;
+  }
 };
